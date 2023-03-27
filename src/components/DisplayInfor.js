2ecc0d0447d98580a74255ce332919c4149048c7
BuiCongMinh <minhvnvnvn1998@gmail.com>
2023-03-27 15:03:14 +0700

    chap 3: react đai dien vơi hook

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,58 +1,112 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './DisplayInfor.scss';
 import logo from "../logo.svg";
 
-class DisplayInfor extends React.Component {
+// class DisplayInfor extends React.Component {
 
-    constructor(props){
-        super(props)
-        this.state = {
-            condition: true
+//     constructor(props) {
+//         console.log('>>> this is constructor ');
+//         super(props)
+//         this.state = {
+//             condition: true
+//         }
+//     }
+
+//     hideShowList() {
+//         this.setState({
+//             condition: !this.state.condition
+//         })
+//     }
+
+//     componentDidMount() {
+//         console.log('this is component DidMount ');
+//         setTimeout(() => {
+//             document.title = 'web của MVN'
+//         }, 3000)
+//     }
+
+//     componentDidUpdate(prevProps, prevState, snapshot) {
+//         if (this.props.listUser !== prevProps.listUser) {
+//             if (this.props.listUser.length === 5) {
+//                 alert('you have 5 user !')
+//             }
+//         }
+//     }
+
+//     render() {
+//         //destructuring 
+//         const { listUser } = this.props;
+
+//         console.log('this is render');
+
+//         // console.log('check array:', listUser);
+//         return (
+//             <div className="display-infor-container">
+
+//                 {true &&
+//                     <div>
+//                         {listUser.map((user) => {
+//                             return (
+//                                 <div key={user.id} className={Number(user.age) < 18 ? "red" : "green"}>
+//                                     <div>Name: {user.userName}, age: {user.age} </div>
+//                                     <div><button onClick={() => { this.props.handelDeleteUser(user.id) }}>delete</button></div>
+//                                     <div> <hr /> </div>
+//                                 </div>
+//                             )
+
+//                         })}
+//                     </div>
+//                 }
+
+//             </div>
+
+//         )
+//     }
+// }
+
+const DisplayInfor = (props) => {
+    const [isShowHideListUser, setIsShowHideListUser] = useState(true)
+
+    const handelShowHideListUser = () => {
+        setIsShowHideListUser(!isShowHideListUser)
+    }
+
+    const { listUser } = props;
+
+    console.log('>>> call Render !');
+
+    useEffect(() => {
+        if(listUser.length === 0 ){
+            alert('bạn đã hết ký tự !')
         }
-    }
+        console.log('>>> call userEffect !');
+    }, [listUser])
 
-
-    hideShowList() {
-        this.setState({
-            condition: !this.state.condition
-        })
-    }
-
-    render() {
-        //destructuring 
-        const { listUser } = this.props;
-
-        // console.log('check array:', listUser);
-        return (
-            <div className="display-infor-container">
-                <img src={logo} alt='hình ảnh minh hoạ scss'/>
-
-                <div >
-                    <span onClick={() => { this.hideShowList() }}>
-                        {this.state.condition === true ? 'hide list -' : 'show list +'}
-                    </span>
-                </div>
-
-                {
-                    (this.state.condition) &&
-                    <div>
-                        {listUser.map((user) => {
-                            return (
-                                <div key={user.id} className={Number(user.age) < 18 ? "red" : "green"}>
-                                    <div>Name: {user.userName}, age: {user.age} </div>
-                                    <div><button onClick={ () => { this.props.handelDeleteUser(user.id) }}>delete</button></div>
-                                    <div> <hr /> </div>
-                                </div>
-                            )
-
-                        })}
-                    </div>
-                }
-
+    return (
+        <div className="display-infor-container">
+            <div>
+                <span onClick={() => { handelShowHideListUser() }}>
+                    {isShowHideListUser ? 'Hide List User -' : 'Show List User +'}
+                </span>
             </div>
+            {isShowHideListUser &&
+                <div>
+                    {listUser.map((user) => {
+                        return (
+                            <div key={user.id} className={Number(user.age) < 18 ? "red" : "green"}>
+                                <div>Name: {user.userName}, age: {user.age} </div>
+                                <div><button onClick={() => { props.handelDeleteUser(user.id) }}>delete</button></div>
+                                <div> <hr /> </div>
+                            </div>
+                        )
 
-        )
-    }
+                    })}
+                </div>
+            }
+
+        </div>
+
+    )
 }
 
 export default DisplayInfor
